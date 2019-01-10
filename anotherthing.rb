b4e5e3fe13e494b66f571ea748e085f1f7c824e1
Nick <nkarsant@gmail.com>
2019-01-09 21:35:27 -0800

    Hello git

---
+++ anotherthing.rb
@@ -0,0 +1,11 @@
+
+"Enter a number"
+n = gets.chomp
+if  n <53
+	puts "that's weak"
+	
+elsif  53 < n < 250
+	"comendable"
+elsif n > 250
+	"ok thats a big one"
+end
---
+++ onemore.rb
@@ -0,0 +1,7 @@
+
+
+puts "Merry CHristmas you filthy animal"
+\n
+puts "bang bang bang bang bang bang"
+\n
+puts "and a happy new year"
